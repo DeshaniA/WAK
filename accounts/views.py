f4df71e958fb2c69d073c535ffde31cb7fc810c9
wasanaGit <wasana1597official@gmail.com>
2020-10-11 20:01:58 +0530

    I render the data to dashboard, status and contract templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
 # Create your views here.
+from .models import *
+
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    contracts = Contract.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_contracts = contracts.count()
+    finished = contracts.filter(status='Finished').count()
+    pending = contracts.filter(status='Pending').count()
+
+    context = {'contracts': contracts, 'customers': customers,
+               'total_contracts': total_contracts, 'finished': finished,
+               'pending': pending}
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def contracts(request):
-    return render(request, 'accounts/contracts.html')
+    services = Service.objects.all()
+
+    return render(request, 'accounts/contracts.html', {'services': services})
 
 
 def customer(request):
